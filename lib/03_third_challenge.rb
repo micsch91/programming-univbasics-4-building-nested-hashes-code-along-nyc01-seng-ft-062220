ca27b734b4d95d58ff78c11c54c5b7b7c89377b9
Michael Schneider <mic.sch91@gmail.com>
2020-06-02 18:15:45 +0000

    Done.

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -10,10 +10,8 @@ def third_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"
         #your key/value pairs here
         },
-      :hero_friends => [
-        {name: "Benvolio", age: "17", attitude: "worried"},
-        {name: "Mercutio", age: "18", attitude: "hot-headed"}
-      ]
+      :hero_friends => []
+
    },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"
@@ -25,10 +23,7 @@ def third_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"
         #your key/value pairs here
         },
-      :heroine_friends => [
-        {name: "Steven", age: "30", attitude: "confused"},
-        {name: "Nurse", age: "44", attitude: "worried"}
-      ]
+      :heroine_friends => []
    }
   }
 
